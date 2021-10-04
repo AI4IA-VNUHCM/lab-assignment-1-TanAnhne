f611fc405ea21ff5bb18168f70a32f1e9a213b3e
TanAnhne <kunbin9999@gmail.com>
2021-10-04 22:37:32 +0700

    Update Exercise3.c

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -7,7 +7,6 @@ Ex:
 | Output: 2 3 5 7                    |
 |____________________________________|
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -16,6 +15,24 @@ int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	//Your codes here
+		int n,b,i,m; 
+	 
+	m=10; 
+	for(b=2;b<=m;b++) 
+	{ 
+	 
+ 	 i=2; 
+ 	while(b%i!=0) 
+  	{ 
+  		i++; 
+  	} 
+  	 
+ 	if(i==b) 
+  	{ 
+		printf("%d ",b) ; 
+		}
+	}
+	
 	
 	return 0;
 }
